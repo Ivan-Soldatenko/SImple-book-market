a335d9cf39c6d25fc3064262e47bc042b9f448af
Ivan <soldatenko_ivan@inbox.ru>
2020-11-01 16:01:29 +0300

    Add Router for urls

--- my_books/urls.py
+++ my_books/urls.py
@@ -2,11 +2,9 @@
 
 from django.urls import path
 from rest_framework.routers import SimpleRouter
-
 from my_books import views
 
 app_name = "my_books"
-
 router = SimpleRouter()
 router.register('books', views.BooksView)
 
