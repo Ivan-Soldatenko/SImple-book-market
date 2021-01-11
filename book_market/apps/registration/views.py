42749dd566a32f341611185ce80273f85b257885
Gashicage <soldatenko_ivan@inbox.ru>
2021-01-11 18:30:51 +0300

    Updated views and urls for User and UserProfile instances

--- book_market/apps/registration/views.py
+++ book_market/apps/registration/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from django.contrib.auth.models import User
 
-# Create your views here.
+from apps.registration.models import UserProfile
+from apps.registration.serializers import UserSerializer, UserProfileSerializer
+
+
+class UserViewSet(viewsets.ReadOnlyModelViewSet):
+    """ViewSet for User model"""
+
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+class UserProfileViewSet(viewsets.ReadOnlyModelViewSet):
+    """ViewSet for UserProfile model"""
+
+    queryset = UserProfile.objects.all()
+    serializer_class = UserProfileSerializer
