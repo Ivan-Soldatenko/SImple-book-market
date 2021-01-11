20959171fba347b5ce3a41580b2b335ecd406308
Gashicage <soldatenko_ivan@inbox.ru>
2021-01-11 18:12:38 +0300

    Added new UserProfile model and create signal for User's instance

--- book_market/apps/registration/models.py
+++ book_market/apps/registration/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
-# Create your models here.
+from django.contrib.auth.models import User
+
+
+class UserProfile(models.Model):
+    """Represent user's profile"""
+
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="profile")
+    is_valid = models.BooleanField(default=False)
+    information = models.TextField(null=True, blank=True)
+    date_joined = models.DateTimeField(auto_now_add=True)
+
+
+@receiver(post_save, sender=User)
+def create_userprofile(sender, instance, created, **kwargs):
+    """Function create user's profile after creating new user's instance"""
+
+    if created:
+        UserProfile.objects.create(user=instance)
