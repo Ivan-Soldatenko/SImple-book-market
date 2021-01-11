4b370909893b1b75a0cd196d7db0490e2a7ea39e
Gashicage <soldatenko_ivan@inbox.ru>
2021-01-11 18:53:54 +0300

    SignupView to the API and urls for it. Updated serializers

--- book_market/apps/registration/views.py
+++ book_market/apps/registration/views.py
@@ -1,5 +1,7 @@
 from rest_framework import viewsets
 from django.contrib.auth.models import User
+from rest_framework.response import Response
+from rest_framework import status
 
 from apps.registration.models import UserProfile
 from apps.registration.serializers import UserSerializer, UserProfileSerializer
@@ -17,3 +19,34 @@ class UserProfileViewSet(viewsets.ReadOnlyModelViewSet):
 
     queryset = UserProfile.objects.all()
     serializer_class = UserProfileSerializer
+
+
+class SignUpView(viewsets.GenericViewSet):
+    """View for registration on the API for users"""
+
+    serializer_class = UserSerializer
+
+    def list(self, request, *args, **kwargs):
+        """Function for represent information about view for users"""
+
+        data = {
+            "information": "You need to fill fields below with the correct information"
+        }
+        return Response(data=data, status=status.HTTP_200_OK)
+
+    def post(self, request, *args, **kwargs):
+        """Function create new user instance"""
+
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+
+        data = {
+            "information": "You are successfully registered on the book-market API"
+        }
+        return Response(data=data, status=status.HTTP_201_CREATED)
+
+    def perform_create(self, serializer):
+        """Function works when user send correct information"""
+
+        serializer.save()
