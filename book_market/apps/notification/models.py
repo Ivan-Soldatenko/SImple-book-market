78b85f90802268ad5c4a0b6043ffdd692923825e
Gashicage <soldatenko_ivan@inbox.ru>
2021-01-11 19:16:53 +0300

    Initial commit for notification app. Added new model to this app

---
+++ book_market/apps/notification/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+
+class BookAddedNotification(models.Model):
+    """
+    Model for storing users, who followed to notification in book-market API
+    Notification will be sent when new books appear in the book-market
+    """
+
+    email = models.EmailField(unique=True)
+
+    def __str__(self):
+        """Return string representation about notification"""
+
+        return self.email
