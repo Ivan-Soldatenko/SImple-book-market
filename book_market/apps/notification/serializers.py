eaa004e0e001050d988a96be190afbf276dbdcd9
Gashicage <soldatenko_ivan@inbox.ru>
2021-01-12 11:45:35 +0300

    Updated serializers

--- book_market/apps/notification/serializers.py
+++ book_market/apps/notification/serializers.py
@@ -1,10 +1,20 @@
 from rest_framework import serializers
+from rest_framework.validators import UniqueValidator
+
 from apps.notification.models import BookAddedNotification
 
 
 class BookAddedNotificationSerializer(serializers.ModelSerializer):
     """Serializer for BookAddedNotification model"""
 
+    email = serializers.EmailField(
+        required=True, validators=[UniqueValidator(queryset=BookAddedNotification.objects.all())]
+    )
+    username = serializers.CharField(
+        required=True, validators=[UniqueValidator(queryset=BookAddedNotification.objects.all())]
+    )
+
     class Meta:
         model = BookAddedNotification
-        fields = ("username", "email")
+        fields = ("username", "email",)
+        read_only_fields = ("username", "email",)
